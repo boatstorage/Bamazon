96bdce35ac7ca711b30022b3bbe3aa55dc1c3392
marina lyulko <marinalyulko@gmail.com>
2017-10-10 18:09:34 -0700

    js file updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,17 +15,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
-  	connection.query('SELECT * from products', function(err, res){
-    if (err) {
-      console.log("whoops:", err);
-      return;
-    }
+  	console.log(afterConnection);
+  	queryAllitems();
+    });
 
+function queryAllitems() {
+  connection.query('SELECT * FROM products', function(err, res){
+    if (err) throw err;
+     
   console.log("--------Bamazon Items------------");
   for (var i = 0; i < res.length; i++){
 
   console.log("item id: " + res[i].item_id + " | name: " + res[i].product_name + " | deptartment: " + res[i].deptartment_name + " | price: " + res[i].price);
-	}
+	};
   connection.end();
-  });
-})
+
+});
+};
+
+function userPrompt() {
+	inquirer
+		.prompt(
+		{
+			name: "item id",
+			type: "input",
+			message: "what is the item ID of the item that you would like to buy?"
+
+		},
+		{
+			name: "units",
+			type: "input",
+			message: "how many would you like to buy?"
+		}
+		)
+}
+
+
